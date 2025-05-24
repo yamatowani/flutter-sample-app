a76a1b11792e58e4b5b7ac3488f0196cd45def2c
yamatowani <festajaune.18637@icloud.com>
2025-05-24 22:18:41 +0900

    add: textField

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_study/screens/search_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: '.env')
+  await dotenv.load(fileName: '.env');
   runApp(const MainApp());
 }
 
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_study/models/article.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -14,7 +18,43 @@ class _SearchScreenState extends State<SearchScreen> {
       appBar: AppBar(
         title: const Text('Qiita Search'),
       ),
-      body: Container(),
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 12,
+            horizontal: 36,
+          ),
+          child: TextField(
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.black,
+            ),
+            decoration: InputDecoration(
+              hintText: '検索ワードを入力してください',
+            ),
+             onSubmitted: (String value) {
+              print(value);
+            },
+          ),
+        ),
+      ],),
     );
+  }
+
+  Future<List<Article>> searchQiita(String keyword) async {
+    final uri = Uri.https('qiita.com', '/api/v2/items', {
+    'query': 'title:$keyword',
+    'per_page': '10',
+    });
+    final String token = dotenv.env['QIITA_ACCESS_TOKEN'] ?? '';
+    final http.Response res = await http.get(uri, headers: {
+      'Authorization': 'Bearer $token',
+    });
+    if (res.statusCode == 200) {
+      final List<dynamic> body = jsonDecode(res.body);
+      return body.map((dynamic json) => Article.fromJson(json)).toList();
+    } else {
+      return [];
+    }
   }
 }
