3a3fd9a9749bdc45b14808a77eaf72b366c549ac
yamatowani <festajaune.18637@icloud.com>
2025-05-24 21:31:24 +0900

    add: ArticleModel

---
+++ lib/models/article.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_study/models/user.dart';
+
+class Article {
+  Article({
+    required this.title,
+    required this.user,
+    this.likesCount = 0,
+    this.tags = const [],
+    required this.createdAt,
+    required this.url,
+  });
+
+  final String title;
+  final User user;
+  final int likesCount;
+  final List<String> tags;
+  final DateTime createdAt;
+  final String url;
+}
